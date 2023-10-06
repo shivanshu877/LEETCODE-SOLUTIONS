24723887f80e3f0f35af334123548b7589aef2b7
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-10-06 09:18:29 +0530

    Time: 0 ms (100%), Space: 6.3 MB (53.64%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if (n <= 3) return n - 1;
-        int quotient = n / 3, remainder = n % 3;
-        if (remainder == 0) return pow(3, quotient);
-        if (remainder == 1) return pow(3, quotient - 1) * 4;
-        return pow(3, quotient) * 2;
+        if(n == 2){
+            return 1;
+        }
+
+        if(n == 3){
+            return 2;
+        }
+
+        int product = 1;
+        while(n > 4){
+            product *= 3;
+            n -= 3;
+        }
+
+        product *= n;
+        
+        return product;
     }
 };
