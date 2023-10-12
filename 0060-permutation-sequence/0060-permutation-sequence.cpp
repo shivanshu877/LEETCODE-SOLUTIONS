43ca12bd417c6a9562f911f264d5bb723be18ec1
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-10-13 00:04:47 +0530

    Time: 0 ms (100%), Space: 6.5 MB (10.37%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> ans(n) ;
-        iota (begin(ans), end(ans), 1); 
-        do {
-            k--;
+        int fact = 1;
+        vector<int>v;
+        for(int i=1;i<n;i++) {
+            v.push_back(i);
+            fact *= i;
         }
-        while(  (k != 0 )  && (next_permutation(ans.begin() , ans.end())));
-       string s ;
-       for(auto x : ans) {
-           s += to_string(x);
-       }
-    return s;
+        v.push_back(n);
+        k -= 1;
+        string ans;
+        while(1) {
+            ans += to_string(v[k/fact]);
+            v.erase(v.begin() + k/fact);
+            if(v.empty()) break;
+            k %= fact;
+            fact /= v.size();
         }
+
+        return ans;
+    }
 };
