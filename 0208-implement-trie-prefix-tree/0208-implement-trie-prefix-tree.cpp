1808f462a63c0bd9628993db5455efc405538435
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-10-19 15:12:04 +0530

    Time: 49 ms (66.21%), Space: 43.8 MB (43.75%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,33 +1,38 @@
-class Trie {
+class TrieNode {
 public:
- set<string> sat ;
-    Trie() {
-        
-    }
-    
-    void insert(string word) {
-             sat.insert(word) ;
-    }
-    
-    bool search(string word) {
-         auto pos =   sat.find(word);
-           if (pos != sat.end())
-              return true;
-	        else
-            return false ;
-    }
-    
-    bool startsWith(string prefix) {
-         for(auto x: sat)
-            if(x.starts_with(prefix)) return true;
-            return false;
+    TrieNode *child[26];
+    bool isWord;
+    TrieNode() {
+        isWord = false;
+        for (auto &a : child) a = nullptr;
     }
 };
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
+class Trie {
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(string s) {
+        TrieNode *p = root;
+        for (auto &a : s) {
+            int i = a - 'a';
+            if (!p->child[i]) p->child[i] = new TrieNode();
+            p = p->child[i];
+        }
+        p->isWord = true;
+    }
+    bool search(string key, bool prefix=false) {
+        TrieNode *p = root;
+        for (auto &a : key) {
+            int i = a - 'a';
+            if (!p->child[i]) return false;
+            p = p->child[i];
+        }
+        if (prefix==false) return p->isWord;
+        return true;
+    }
+    bool startsWith(string prefix) {
+        return search(prefix, true);
+    }
+};
