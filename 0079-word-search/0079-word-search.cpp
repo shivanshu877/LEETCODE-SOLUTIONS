3802d226ea44f3c02ca3f1f0ac6f131e57cae85d
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-10-26 14:46:06 +0530

    Time: 472 ms (61.06%), Space: 8.3 MB (48.08%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,54 +1,23 @@
 class Solution {
-private:
-    
-    int dx[4] = {-1, 0, 1, 0};
-    int dy[4] = {0, 1, 0, -1};
-    
-    bool isValid(int row, int col, int n, int m) {
-        return row >= 0 and row < n and col >= 0 and col < m;
-    }
-    
-    bool findWord(int row, int col, int ind, vector<vector<char>> &grid, 
-                  vector<vector<bool>> &vis, string &word) {
-        
-        if (ind == word.size()) {
-            return true;
-        }
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        if (!isValid(row, col, n, m) or word[ind] != grid[row][col] or vis[row][col]) 
-            return false;
-        
-        vis[row][col] = true;
-        
-        for (int i = 0; i < 4; i++) {
-            int newRow = row + dx[i];
-            int newCol = col + dy[i];
-            
-            bool isPossible = findWord(newRow, newCol, ind + 1, grid, vis, word);
-            
-            if (isPossible) 
-                return true;
-        }
-        
-        return vis[row][col] = false;
-    }
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        
-        vector<vector<bool>> vis(n, vector<bool>(m));
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (findWord(i, j, 0, board, vis, word)) 
-                    return true;
-            }
-        }
-        
+   bool exist(vector<vector<char>>& board, string word) {
+    for (unsigned int i = 0; i < board.size(); i++) 
+        for (unsigned int j = 0; j < board[0].size(); j++) 
+            if (dfs(board, i, j, word))
+                return true;
+    return false;
+}
+
+bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
+    if (!word.size())
+        return true;
+    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
         return false;
-    }
+    char c = board[i][j];
+    board[i][j] = '*';
+    string s = word.substr(1);
+    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
+    board[i][j] = c;
+    return ret;
+}
 };
