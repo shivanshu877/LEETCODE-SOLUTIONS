e57ef45557b3dd195072e25235f7ff19bb38ebcc
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-10-27 11:54:38 +0530

    Time: 22 ms (38.61%), Space: 28.3 MB (26.07%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isLeaf(TreeNode* curr) {
+        return !curr->left && !curr->right;
+    }
+    int getMaxAns(TreeNode* curr, int& max_ans) {
+        if(!curr) return 0;
+        if(isLeaf(curr)) {
+            max_ans = max(max_ans, curr->val);
+            return curr->val;
+        }
+        int leftAns = getMaxAns(curr->left, max_ans);
+        int rightAns = getMaxAns(curr->right, max_ans);
+        max_ans = max(max_ans, max(curr->val, max(leftAns, max(rightAns, leftAns + rightAns)) + curr->val));
+        return max(curr->val, max(leftAns, rightAns) + curr->val);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        if(isLeaf(root))
+            return root->val;
+        int max_ans = INT_MIN;
+        getMaxAns(root, max_ans);
+        return max_ans;
+    }
+};
