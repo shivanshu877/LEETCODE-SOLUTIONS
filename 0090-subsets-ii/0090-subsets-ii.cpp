4e516a58fba1f1814f709679ac8710ffc4a2d134
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-11-06 15:43:45 +0530

    Time: 10 ms (9.96%), Space: 17.4 MB (17.44%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-   
-void backTracking(set<vector<int>> &res, const vector<int> &nums, int i, int sz, vector<int> sentr)
-{
-    if (i == sz)
-    {
-        sort(sentr.begin(), sentr.end()); 
-        res.insert(sentr);
+ void fun(vector < int > & nums, int index, vector < int > ds, set < vector < int >> & res) {
+      if (index >= nums.size()) {
+          cout<< index << " " ;
+        sort(ds.begin(), ds.end());
+        res.insert(ds);
         return;
+      }
+      ds.push_back(nums[index]);
+      fun(nums, index + 1, ds, res);
+      ds.pop_back();
+      fun(nums, index + 1, ds, res);
     }
-
-    sentr.push_back(nums[i]);
-    backTracking(res, nums, i + 1, sz, sentr);
-    sentr.pop_back();
-    backTracking(res, nums, i + 1, sz, sentr);
-}
-
-vector<vector<int>> subsetsWithDup(vector<int> &nums)
-{
-   
-    set<vector<int>> res;
-    int sz = nums.size();
-    vector<int> sent;
-    backTracking(res, nums, 0, sz, sent);
-    vector<vector<int>> res2;
-    for (auto vec : res)
-        res2.push_back(vec);
-    return res2;
-}
-
+  vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+    vector < vector < int >> ans;
+    set < vector < int >> res;
+    vector < int > ds;
+    fun(nums, 0, ds, res);
+    for (auto it = res.begin(); it != res.end(); it++) {
+      ans.push_back( * it);
+    }
+    return ans;
+  }
 };
