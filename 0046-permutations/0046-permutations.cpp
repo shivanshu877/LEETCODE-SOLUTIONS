fdc00f57f5604059dbca14e2f0f46fee73029a45
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-11-07 15:40:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (29.54%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,34 +1,18 @@
-class Solution
-{
-    private:
-        void solver(vector<int> &input, vector<int> &output, vector< vector< int >> &ans, vector< int > freqq)
-        {
-            if (output.size() == input.size())
-            {
-                ans.push_back(output);
-                return;
-            }
-
-            for (int i = 0; i < input.size(); i++)
-            {
-                if (!freqq[i])
-                {
-                    output.push_back(input[i]);
-                    freqq[i] = 1;
-                    solver(input, output, ans, freqq);
-                    freqq[i] = 0;
-                    output.pop_back();
-                }
-            }
-        }
-    public:
-
-        vector<vector < int>> permute(vector<int> &nums)
-        {
-            vector<vector < int>> ans;
-            vector<int> output;
-            vector<int> freqq(nums.size(), 0);
-            solver(nums, output, ans, freqq);
-            return ans;
-        }
+class Solution {
+public:
+    void solver(int index , vector<vector<int>>& ans , vector<int> nums) {
+       if(index == nums.size()){
+           ans.push_back( nums) ;
+       }
+       for( int i =  index  ; i< nums.size() ; i++) {
+       swap(nums[index] , nums[i]) ;
+       solver(index+1 , ans , nums);
+       swap(nums[index] , nums[i]) ;
+       }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans ;
+        solver(0 , ans , nums);
+        return ans;
+    }
 };
