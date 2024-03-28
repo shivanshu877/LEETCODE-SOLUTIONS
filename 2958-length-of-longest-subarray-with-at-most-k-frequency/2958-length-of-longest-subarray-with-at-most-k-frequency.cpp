fa08ce1a53a21d293cc69663c4fd41b3b119b7ce
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-03-28 08:59:00 +0530

    Time: 223 ms (39.41%), Space: 148.5 MB (57.21%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -3,20 +3,14 @@ public:
     int maxSubarrayLength(vector<int>& nums, int k) {
         unordered_map<int , int> ans ;
         int returnvalue = INT_MIN;
-        int left =0 ;
+        int left = -1 ;
         for( int right = 0 ;  right < nums.size() ; right++) {
             ans[nums[right]]++;
-                if(ans[nums[right]] > k) {
-                    while(nums[left]!= nums[right] && left < right){ 
-                    ans[nums[left]]--;
-                    if(ans[nums[left]] <= 0)ans.erase(nums[left]);
-                    left++ ;
-                    }
-                    ans[nums[left]]--;
-                    if(ans[nums[left]] <= 0)ans.erase(nums[left]);
-                    left++;                    
+                while(ans[nums[right]] > k) {
+                   left++;
+                   ans[nums[left]]--;
                 }
-             returnvalue = max( (right -left +1) , returnvalue) ;
+             returnvalue = max( (right -left ) , returnvalue) ;
             }
         
         return returnvalue;
