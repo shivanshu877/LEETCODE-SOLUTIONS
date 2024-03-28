48410ba777f82942f98c002019f480100f23c21f
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-03-28 09:15:59 +0530

    Time: 54 ms (40.59%), Space: 48.4 MB (90.68%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+    var map = function(arr, fn) {
+  const transformedArr = [];
+  let index = 0;
+  for (const element of arr) {
+    transformedArr[index] = fn(element, index);
+    index++;
+  }
+  return transformedArr;
+};
