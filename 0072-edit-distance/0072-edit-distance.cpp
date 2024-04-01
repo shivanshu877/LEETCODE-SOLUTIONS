f61b50b2a41567c8300786a69d0ab726647bc48a
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-04-01 03:05:43 -0700

    Time: 7 ms (64.25%), Space: 12 MB (54.68%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    
+    int lcs(string s1, string s2) {
+    int n = s1.size();
+    int m = s2.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // Create a DP table
+
+    // Initialize the base cases
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = 0;
+    }
+    for (int i = 0; i <= m; i++) {
+        dp[0][i] = 0;
+    }
+
+    // Fill in the DP table to calculate the length of LCS
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
+            else
+                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match, consider the maximum from left or above
+        }
+    }
+
+    return dp[n][m]; // Return the length of the Longest Common Subsequence
+}
+    int minDistance(string word1, string word2) {
+const int m = word1.length();//first word length
+    const int n = word2.length();//second word length
+    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+    for (int i = 1; i <= m; ++i)
+      dp[i][0] = i;
+
+    for (int j = 1; j <= n; ++j)
+      dp[0][j] = j;
+
+    for (int i = 1; i <= m; ++i)
+      for (int j = 1; j <= n; ++j)
+        if (word1[i - 1] == word2[j - 1])//same characters
+          dp[i][j] = dp[i - 1][j - 1];//no operation
+        else
+          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+                             //replace       //delete        //insert
+    return dp[m][n];    }
+};
