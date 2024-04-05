d024f3d0de6ce0555f1b08b55367c18e483ffefe
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-04-05 10:02:44 -0700

    Time: 134 ms (57.59%), Space: 87.6 MB (66.83%) - LeetHub

---
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int i=0,j=0;
+        int sum=0,ans = INT_MAX;
+        vector<int>bits(32,0);
+        while(j<nums.size()){
+            sum|=nums[j];
+            for(int z = 0;z<32;z++){
+                if((1<<z) & nums[j])
+                    bits[z]++;
+            }
+            j++;
+            while(i<j && sum>=k){
+                ans = min(ans,j-i);
+                for(int z = 0;z<32;z++){
+                    if((1<<z) & nums[i])
+                    {
+                        bits[z]--;
+                        if(bits[z]==0)
+                            sum-=(1<<z);
+                    }
+                }
+                i++;
+            }
+        }
+        return ans==INT_MAX?-1:ans;
+    }
+};
