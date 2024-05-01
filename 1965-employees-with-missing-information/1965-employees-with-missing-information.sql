a491e8bcdb321b773d882dea0e070bf6a659026c
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-05-01 15:44:16 +0530

    Time: 1277 ms (30.72%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,12 +1,12 @@
-SELECT COALESCE(e.employee_id, s.employee_id) AS employee_id
+SELECT e.employee_id AS employee_id
 FROM Employees e
 LEFT JOIN Salaries s ON e.employee_id = s.employee_id
 WHERE e.name IS NULL OR s.salary IS NULL
 
 UNION
 
-SELECT COALESCE(e.employee_id, s.employee_id) AS employee_id
+SELECT s.employee_id AS employee_id
 FROM Employees e
 RIGHT JOIN Salaries s ON e.employee_id = s.employee_id
 WHERE e.name IS NULL OR s.salary IS NULL
-ORDER BY employee_id; -- Ordering by the alias created in the SELECT
+ORDER BY employee_id;
