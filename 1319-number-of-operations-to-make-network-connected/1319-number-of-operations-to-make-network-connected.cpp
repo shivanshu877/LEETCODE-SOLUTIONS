87323da6288acd4a4752767355b5ec888b9cdd81
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-05-05 16:32:37 +0530

    Time: 96 ms (39.10%), Space: 46.3 MB (47.68%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,5 +1,54 @@
 class Solution {
 public:
+    class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
     void dfs(int node, const vector<vector<int>>& adj, vector<int>& vis) {
         vis[node] = 1;
         for (int x : adj[node]) {
@@ -9,27 +58,27 @@ public:
         }
     }
 
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1)
+    int makeConnected(int n, vector<vector<int>>& edge) {
+         if(edge.size()<n-1)
             return -1;
-        vector<int> vis(n, 0);
-        vector<vector<int>> adj(n);
-        
-        // Build adjacency list
-        for (const auto& conn : connections) {
-            adj[conn[0]].push_back(conn[1]);
-            adj[conn[1]].push_back(conn[0]);
-        }
-        
-        int count = 0;
-        for (int i = 0; i < n; ++i) {
-            if (!vis[i]) {
-                dfs(i, adj, vis);
-                count++;
+        DisjointSet ds(n);
+        int cntExtras = 0;
+        for (auto it : edge) {
+            int u = it[0];
+            int v = it[1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                cntExtras++;
+            }
+            else {
+                ds.unionBySize(u, v);
             }
         }
-        
+        int cntC = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent[i] == i) cntC++;
+        }
+        return   cntC - 1;
        
-        return count -1;
+    
     }
 };
