248165522bbf5801f1643b18653aadcbe1db0f43
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-05-07 16:47:29 +0530

    Time: 1100 ms (29.63%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,5 +1,10 @@
-SELECT id,
-       IF(p_id IS NULL, "Root",
-          IF( id IN  (SELECT  p_id FROM Tree group by p_id having count(*) > 0 ) , 
-         "Inner", "Leaf")) AS type
-FROM Tree;
+SELECT id, 
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (
+            SELECT p_id FROM Tree
+        ) THEN 'Inner'
+        ELSE 'Leaf' 
+    END 
+    AS type
+FROM Tree
