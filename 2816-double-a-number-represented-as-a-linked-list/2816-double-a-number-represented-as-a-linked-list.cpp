c7840853403ffa7ccd84296ca95fae33ce809e96
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-05-07 09:39:08 +0530

    Time: 174 ms (61.96%), Space: 120.7 MB (72.53%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* prev = nullptr;
-
-        // Traverse the linked list
-        while (curr != nullptr) {
-            int twiceOfVal = curr->val * 2;
-
-            // If the doubled value is less than 10
-            if (twiceOfVal < 10) {
-                curr->val = twiceOfVal;
-            } 
-
-            // If doubled value is 10 or greater
-            else if (prev != nullptr) { // other than first node
-                // Update current node's value with units digit of the doubled value
-                curr->val = twiceOfVal % 10;
-                // Add the carry to the previous node's value
-                prev->val = prev->val + 1;
-            } 
-            // If it's the first node and doubled value is 10 or greater
-            else { // first node
-                // Create a new node with carry as value and link it to the current node
-                head = new ListNode(1, curr);
-                // Update current node's value with units digit of the doubled value
-                curr->val = twiceOfVal % 10;
-            }
-
-            // Update prev and curr pointers
-            prev = curr;
-            curr = curr->next;
+        // If the value of the head node is greater than 4, insert a new node at the beginning
+        if (head->val > 4) {
+            head = new ListNode(0, head);
         }
+        
+        // Traverse the linked list
+        for (ListNode* node = head; node != nullptr; node = node->next) {
+            // Double the value of the current node and update it with the units digit
+            node->val = (node->val * 2) % 10;
+            
+            // If the current node has a next node 
+            // and the next node's value is greater than 4,
+            // increment the current node's value to handle carry
+            if (node->next != nullptr && node->next->val > 4) {
+                node->val++;
+            }
+        }
+        
         return head;
     }
 };
