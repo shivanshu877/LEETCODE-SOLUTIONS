b993a12e6eb2535cd6e9f80df91b3a3ba63ef230
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-05-07 16:56:56 +0530

    Time: 1498 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,11 @@
-select s.id , 
-  CASE
-  WHEN id = (select max(id) from seat) AND mod( (select count(*) from seat ), 2) = 1    THEN s.student 
-  WHEN mod(id,2) = 1 THEN ( select student from seat where id = s.id+1) 
-  ELSE ( select student from seat where id = s.id-1)
-  END as student
-  FROM seat s
-  ORDER BY id 
-  ;
-   
+SELECT
+    CASE
+        WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1
+            THEN id
+        WHEN id % 2 = 1
+            THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
