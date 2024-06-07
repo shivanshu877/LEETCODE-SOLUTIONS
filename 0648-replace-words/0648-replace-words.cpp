99360c88fefb8bc6a90b6898a19df9005ef9c7f7
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-06-07 08:15:25 +0530

    Time: 77 ms (73.02%), Space: 81.5 MB (40.81%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,39 +1,66 @@
+class TrieNode {
+public:
+    bool isEnd;
+    vector<TrieNode*> children;
+
+    TrieNode() : children(26, nullptr) { isEnd = false; }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() { root = new TrieNode(); }
+
+    void insert(string word) {
+        TrieNode* current = root;
+        for (char c : word) {
+            if (current->children[c - 'a'] == nullptr) {
+                current->children[c - 'a'] = new TrieNode();
+            }
+            current = current->children[c - 'a'];
+        }
+        current->isEnd = true;
+    }
+
+    // Find the shortest root of the word in the trie
+    string shortestRoot(string word) {
+        TrieNode* current = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word[i];
+            if (current->children[c - 'a'] == nullptr) {
+                // There is not a corresponding root in the trie
+                return word;
+            }
+            current = current->children[c - 'a'];
+            if (current->isEnd) {
+                return word.substr(0, i + 1);
+            }
+        }
+        // There is not a corresponding root in the trie
+        return word;
+    }
+};
+
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<string , int> dict ;
-        for(auto x : dictionary){
-            dict[x]++;
+        istringstream sStream(sentence);
+
+        Trie dictTrie;
+        for (string word : dictionary) {
+            dictTrie.insert(word);
         }
-        vector<string> words ;
-        string temp = "" ;
-        for(auto it : sentence){
-            if(it == ' '){
-             words.push_back(temp);
-              temp = "" ;
-            }
-            else
-            temp += it ;
-            
+
+        // Replace each word in sentence with the corresponding shortest root
+        string word;
+        string newSentence;
+        while (getline(sStream, word, ' ')) {
+            newSentence += dictTrie.shortestRoot(word) + " ";
         }
-        if(temp.size()) words.push_back(temp);
-        for(int i = 0 ; i< words.size() ;i++) {
-            auto x = words[i];
-            string t = "" ;
-        for(auto y : x) {
-            t+=y;
-            if(dict.find(t) != dict.end()){
-                words[i] = t ;
-                break ;
-                }
-            }
-        }
-         temp = "";
-        for(int i = 0 ; i< words.size() ;i++){
-            auto it  = words[i] ;
-            temp+=it ;
-            if(i != words.size() - 1 )temp+=" "; 
-        }
-        return temp ;
+
+        newSentence.pop_back();  // remove extra space
+        return newSentence;
     }
 };
