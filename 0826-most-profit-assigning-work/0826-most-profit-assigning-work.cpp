67dd100b9d91cf3a73c7a3171d0e800e6a88b605
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-06-18 09:41:50 +0530

    Time: 61 ms (39.05%), Space: 41 MB (72.17%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        reverse(worker.begin() , worker.end());
-                reverse(difficulty.begin() , difficulty.end());
-                reverse(profit.begin() , profit.end());
-            int ans = 0 ;
-          int n = worker.size() ;
-        for(int i =0  ; i< n ; i++ ) {
-            int temp = worker[i];
-          int maxi = 0 ;
-            for(int j =0 ; j < difficulty.size() ; j++){
-                 int diff = difficulty[j] ;
-                if(temp >= diff){
-                   maxi = max(maxi , profit[j]);
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
+        vector<pair<int, int>> jobProfile;
+        jobProfile.push_back({0, 0});
+        for (int i = 0; i < difficulty.size(); i++) {
+            jobProfile.push_back({difficulty[i], profit[i]});
+        }
+
+        // Sort by difficulty values in increasing order.
+        sort(jobProfile.begin(), jobProfile.end());
+        for (int i = 0; i < jobProfile.size() - 1; i++) {
+            jobProfile[i + 1].second =
+                max(jobProfile[i].second, jobProfile[i + 1].second);
+        }
+
+        int netProfit = 0;
+        for (int i = 0; i < worker.size(); i++) {
+            int ability = worker[i];
+
+            int l = 0, r = jobProfile.size() - 1, jobProfit = 0;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (jobProfile[mid].first <= ability) {
+                    jobProfit = max(jobProfit, jobProfile[mid].second);
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
                 }
             }
-            if(maxi) ans +=maxi;
+
+            // Increment profit of current worker to total profit.
+            netProfit += jobProfit;
         }
-        return ans;
+        return netProfit;
     }
 };
