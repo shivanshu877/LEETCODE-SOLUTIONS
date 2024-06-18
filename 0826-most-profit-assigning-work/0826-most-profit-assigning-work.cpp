11d77ddc99223f3f27d0b09f89829831b895f3cd
Shivanshu <112751483+shivanshu877@users.noreply.github.com>
2024-06-18 09:46:58 +0530

    Time: 93 ms (23.45%), Space: 48.4 MB (17.01%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -2,36 +2,20 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
                             vector<int>& worker) {
-        vector<pair<int, int>> jobProfile;
-        jobProfile.push_back({0, 0});
+        int maxAbility = *max_element(begin(worker), end(worker));
+        vector<int> jobs(maxAbility + 1, 0);
         for (int i = 0; i < difficulty.size(); i++) {
-            jobProfile.push_back({difficulty[i], profit[i]});
+            if (difficulty[i] <= maxAbility)
+                jobs[difficulty[i]] = max(jobs[difficulty[i]], profit[i]);
         }
-
-        // Sort by difficulty values in increasing order.
-        sort(jobProfile.begin(), jobProfile.end());
-        for (int i = 0; i < jobProfile.size() - 1; i++) {
-            jobProfile[i + 1].second =
-                max(jobProfile[i].second, jobProfile[i + 1].second);
-        }
+        // Take maxima of prefixes.
+        for (int i = 1; i <= maxAbility; i++)
+            jobs[i] = max(jobs[i], jobs[i - 1]);
 
         int netProfit = 0;
-        for (int i = 0; i < worker.size(); i++) {
-            int ability = worker[i];
-
-            int l = 0, r = jobProfile.size() - 1, jobProfit = 0;
-            while (l <= r) {
-                int mid = (l + r) / 2;
-                if (jobProfile[mid].first <= ability) {
-                    jobProfit = max(jobProfit, jobProfile[mid].second);
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            }
-
-            // Increment profit of current worker to total profit.
-            netProfit += jobProfit;
+        for (auto ability : worker) {
+            int maxProfit = jobs[ability];
+            netProfit += maxProfit;
         }
         return netProfit;
     }
